0ad30c714e64263ea699365a512e09d0866e82ce
Rokoucha <git@rokoucha.net>
2021-10-05 19:39:40 +0900

    feat: 再送制御を賢く

--- src/lib/futaba/index.ts
+++ src/lib/futaba/index.ts
@@ -2,7 +2,8 @@ import { dirname, join } from 'path'
 import { sleep } from '../../utils'
 import { Threads, threadSchema, threadsSchema } from './schema'
 
-export const FETCH_INTERVAL_MS = 4000 as const // 操作が早すぎます あと2秒で再送できます
+export const FETCH_INTERVAL_MS = 3000 as const
+export const RETRY_MAX = 5 as const
 
 export type { Response, Thread, Threads } from './schema'
 
@@ -53,29 +54,20 @@ export class FutabaClient {
 
     const matches = Array.from(text.matchAll(regex))
 
+    if (matches.length < 3) throw new Error(text)
+
     return matches.map(([_, url, title]) => ({
       url: `${bbsmenuUrl.protocol}${url}`,
       title,
     }))
   }
 
-  async #fetch(
-    input: string,
-    init?: RequestInit | undefined,
-  ): Promise<Response> {
-    const elapsedTime = new Date().getTime() - this.#fetchedAt.getTime()
+  get fetchedAt() {
+    return this.#fetchedAt
+  }
 
-    if (elapsedTime < FETCH_INTERVAL_MS) {
-      console.log('FutabaClient', 'throttling', elapsedTime, 'ms')
-
-      await sleep(FETCH_INTERVAL_MS)
-    }
-
-    const res = await fetch(join(this.#baseUrl, input), init)
-
-    this.#fetchedAt = new Date()
-
-    return res
+  set fetchedAt(value: Date) {
+    this.#fetchedAt = value
   }
 
   stats() {
@@ -86,21 +78,60 @@ export class FutabaClient {
     }
   }
 
+  async #fetch(
+    input: string,
+    init?: RequestInit | undefined,
+    retry = 0,
+  ): Promise<Response> {
+    const elapsedTime = new Date().getTime() - this.#fetchedAt.getTime()
+
+    if (elapsedTime < FETCH_INTERVAL_MS) {
+      const wait = FETCH_INTERVAL_MS * (retry === 0 ? 1 : retry) - elapsedTime
+
+      console.log(
+        'FutabaClient',
+        'elapsed',
+        elapsedTime,
+        'ms,',
+        'throttling',
+        wait,
+        'ms',
+      )
+
+      await sleep(wait)
+    }
+
+    const res = await fetch(join(this.#baseUrl, input), init)
+
+    this.#fetchedAt = new Date()
+
+    if (!res.headers.get('Content-Type')?.startsWith('application/json')) {
+      const message = this.#decoder.decode(await res.arrayBuffer())
+
+      if (
+        /操作が早すぎます あと2秒で再送できます/g.test(message) &&
+        retry < RETRY_MAX
+      ) {
+        ++retry
+
+        console.log('FutabaClient', 'retry', retry, '/', RETRY_MAX)
+
+        return this.#fetch(input, init, retry)
+      }
+
+      throw new Error(message)
+    }
+
+    return res
+  }
+
   async threads() {
     const res = await this.#fetch('futaba.php?mode=json')
     if (!res.ok) {
       throw new Error('Failed to fetch threads')
     }
 
-    const resErr = res.clone()
-    const text = await res.text()
-
-    let threads
-    try {
-      threads = threadsSchema.parse(JSON.parse(text))
-    } catch (_) {
-      throw new Error(this.#decoder.decode(await resErr.arrayBuffer()))
-    }
+    const threads = threadsSchema.parse(await res.json())
 
     this.#threads = threads
 
@@ -128,15 +159,7 @@ export class FutabaClient {
       throw new Error('Failed to fetch responses')
     }
 
-    const resErr = res.clone()
-    const text = await res.text()
-
-    let thread
-    try {
-      thread = threadSchema.parse(JSON.parse(text))
-    } catch (_) {
-      throw new Error(this.#decoder.decode(await resErr.arrayBuffer()))
-    }
+    const thread = threadSchema.parse(await res.json())
 
     let index = this.#threads.res.findIndex(
       (t) => t.res[0].resId === params.res,
